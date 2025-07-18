113dd0853b2c419fecebf4d1fd030ea0e5b78847
shayla-develops-webs <shaylamrodgers@gmail.com>
2025-07-18 07:44:15 -0500

    remove stream.io. It was not lightweight and overkill for the assessment reqirements. Use custom ui instead

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,63 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { environment } from '../environments/environment';
 import { RouterOutlet } from '@angular/router';
-import { TranslateModule } from '@ngx-translate/core';
-import { User } from 'stream-chat';
-import {
-  ChatClientService,
-  ChannelService,
-  StreamI18nService,
-  StreamAutocompleteTextareaModule,
-  StreamChatModule,
-} from 'stream-chat-angular';
+import { env } from 'process';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    TranslateModule,
-    StreamAutocompleteTextareaModule,
-    StreamChatModule,
-    RouterOutlet,
-  ],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss',
+  styleUrls: ['./app.scss'],
 })
-export class App implements OnInit {
-  constructor(
-    private chatService: ChatClientService,
-    private channelService: ChannelService,
-    private streamI18nService: StreamI18nService
-  ) {
-    const apiKey = 'dz5f4d5kzrue';
-    const userId = 'weathered-cell-2';
-    const userToken =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoid2VhdGhlcmVkLWNlbGwtMiIsImV4cCI6MTc1MjgxOTIyMn0.TjsmtKXjMGheT0aii4M_vf_81ZPNPapSterpLSdS2uU';
-    const userName = 'weathered';
-
-    const user: User = {
-      id: userId,
-      name: userName,
-      image: `https://getstream.io/random_png/?name=${userName}`,
-    };
-
-    this.chatService.init(apiKey, user, userToken);
-    this.streamI18nService.setTranslation();
-  }
-
-  async ngOnInit() {
-    const channel = this.chatService.chatClient.channel(
-      'messaging',
-      'talking-about-angular',
-      {
-        // add as many custom fields as you'd like
-        image: 'assets/icon_angular_gradient_alt01.png',
-        name: 'Talking about Angular',
-      } as any
-    );
-    await channel.create();
-    this.channelService.init({
-      type: 'messaging',
-      id: { $eq: 'talking-about-angular' },
-    });
-  }
+export class App {
+  protected title = 'hey-chat';
 }
